a395e8b739e5329b31b2fcceb516ff48a9f870a4
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-12-29 10:17:34 +0545

    fix: update time

--- utils/cron.js
+++ utils/cron.js
@@ -3,7 +3,7 @@ import cron from "node-cron";
 export async function scheduleEvent(callback, time) {
   cron.schedule(
     // Schedule the function to run at 10 AM from Sunday to Friday
-    "0 10 * * 0-5",
+    "45 9 * * 0-5",
     () => {
       callback();
     },
